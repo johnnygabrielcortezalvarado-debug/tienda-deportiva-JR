8337defb56cbdc2e8664ff116ab446f3acba6ef2
johnnygabrielcortezalvarado-depuración <johnnygabrielcortezalvaradoq@gmail.com>
2026-07-07 09:44:26 -0500

    Ver errores

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// =============================================
 // ============================================================
 // public/index.php — Punto de entrada y enrutador MVC
 // Tienda de Ropa Deportiva
